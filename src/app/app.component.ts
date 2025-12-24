9a0ec21a23543773f1cbe8e3d351f0968050d41f
gajanankdm <gajanankadam0965@gmail.com>
2025-12-25 00:53:39 +0530

    ok

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Icards } from './shared/models/cards';
+import { productsArr } from './shared/const/card';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,5 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'cardtask2';
+  productsArr1:Array<Icards>=productsArr
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,20 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MatButtonModule,
+    
+    MatCardModule,
+         BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
